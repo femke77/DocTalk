e2de923bd3212afffed500a8f54653890a4064e7
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-07-29 18:52:11 -0700

    testing

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import { Link, useHistory } from 'react-router-dom';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -21,29 +18,41 @@ function Signup() {
     role: 'patient', // Default role is patient
   });
 
-  const [addUser] = useMutation(ADD_USER);
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-        role: formState.role, // Include the selected role in the mutation
-      },
-    });
-    const token = mutationResponse.data.addUser.token;
-    Auth.login(token);
-  };
-
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
       ...formState,
       [name]: value,
     });
+  };
+
+  const history = useHistory(); 
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: formState.email,
+          password: formState.password,
+          firstName: formState.firstName,
+          lastName: formState.lastName,
+          role: formState.role,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Signup failed');
+      }
+      history.push('/'); // Redirect to the Homepage (change the path if needed)
+    } catch (error) {
+      console.error('Signup error:', error);
+      // Handle any errors that occurred during signup
+    }
   };
 
   return (
@@ -152,3 +161,191 @@ function Signup() {
 }
 
 export default Signup;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+// import Auth from '../utils/auth';
+// import { ADD_USER } from '../utils/mutations';
+// import Grid from "@mui/material/Grid";
+// import Box from "@mui/material/Box";
+// import Typography from "@mui/material/Typography";
+// import Container from "@mui/material/Container";
+// import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+// import Button from "@mui/material/Button";
+
+// function Signup() {
+//   const [formState, setFormState] = useState({
+//     email: '',
+//     password: '',
+//     firstName: '',
+//     lastName: '',
+//     role: 'patient', // Default role is patient
+//   });
+
+//   const [addUser] = useMutation(ADD_USER);
+
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
+//     const mutationResponse = await addUser({
+//       variables: {
+//         email: formState.email,
+//         password: formState.password,
+//         firstName: formState.firstName,
+//         lastName: formState.lastName,
+//         role: formState.role, // Include the selected role in the mutation
+//       },
+//     });
+//     const token = mutationResponse.data.addUser.token;
+//     Auth.login(token);
+//   };
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
+
+//   return (
+//     <Container component="main" maxWidth="xs">
+//       <Box
+//         sx={{
+//           marginTop: 8,
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Typography component="h1" variant="h5">
+//           Sign up
+//         </Typography>
+//         <Box component="form" onSubmit={handleFormSubmit} noValidate sx={{ mt: 1 }}>
+//           <TextField
+//             margin="normal"
+//             required
+//             fullWidth
+//             id="firstName"
+//             label="First Name"
+//             name="firstName"
+//             autoComplete="given-name"
+//             autoFocus
+//             onChange={handleChange}
+//           />
+//           <TextField
+//             margin="normal"
+//             required
+//             fullWidth
+//             id="lastName"
+//             label="Last Name"
+//             name="lastName"
+//             autoComplete="family-name"
+//             onChange={handleChange}
+//           />
+//           <TextField
+//             margin="normal"
+//             required
+//             fullWidth
+//             id="email"
+//             label="Email Address"
+//             name="email"
+//             autoComplete="email"
+//             onChange={handleChange}
+//           />
+//           <TextField
+//             margin="normal"
+//             required
+//             fullWidth
+//             name="password"
+//             label="Password"
+//             type="password"
+//             id="password"
+//             autoComplete="new-password"
+//             onChange={handleChange}
+//           />
+//           {/* Radio buttons for Doctor/Patient selection */}
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 name="role"
+//                 value="doctor"
+//                 checked={formState.role === "doctor"}
+//                 onChange={handleChange}
+//               />
+//             }
+//             label="Doctor"
+//           />
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 name="role"
+//                 value="patient"
+//                 checked={formState.role === "patient"}
+//                 onChange={handleChange}
+//               />
+//             }
+//             label="Patient"
+//           />
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             sx={{ mt: 3, mb: 2 }}
+//           >
+//             Sign Up
+//           </Button>
+//           <Grid container>
+//             <Grid item xs>
+//               <Link href="#" variant="body2">
+//                 Forgot password?
+//               </Link>
+//             </Grid>
+//             <Grid item>
+//               <Link to="/login" variant="body2">
+//                 Already have an account? Log in
+//               </Link>
+//             </Grid>
+//           </Grid>
+//         </Box>
+//       </Box>
+//     </Container>
+//   );
+// }
+
+// export default Signup;
