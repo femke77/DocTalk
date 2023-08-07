116b5ef562c7a9cb80f4e03e844b64b83e77a1ac
sehrishkhan336 <44490311+sehrishkhan336@users.noreply.github.com>
2023-08-07 02:05:59 -0700

    adding routing to Homepage from DocTalk

--- client/src/components/Header/PatientHeader.js
+++ client/src/components/Header/PatientHeader.js
@@ -219,13 +219,22 @@ export default function ProfilePrimarySearchAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            DocTalk
-          </Typography>
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              DocTalk
+            </Typography>
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
