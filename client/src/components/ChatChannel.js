3c8607a71363b6d98d8a9c6f7393adff7f9b1384
JoelP-11 <Jp421513@gmail.com>
2023-08-07 12:13:35 -0700

    no more errors with the name and send buttons, now working on getting the name to show above the chat sent

--- client/src/components/ChatChannel.js
+++ client/src/components/ChatChannel.js
@@ -1,11 +1,15 @@
-import { useMutation, useQuery } from "@apollo/client"
-import { CHANNEL_DETAILS } from "../utils/queries"
-import { ADD_CHAT_MESSAGE } from "../utils/mutations"
-import Auth from "../utils/auth"
+import React, { useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import { CHANNEL_DETAILS } from "../utils/queries";
+import { ADD_CHAT_MESSAGE } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 export default function Channel() {
 
     const id = "1"
+
+    const [messageText, setMessageText] = useState("");
+    const [name, setName] = useState("");
 
     const { data, loading, error } = useQuery(CHANNEL_DETAILS, {
         variables: { channelId: id },
@@ -36,18 +40,23 @@ export default function Channel() {
     const handleKeyUp = async (evt) => {
         if (evt.keyCode === 13) {
             // ADD CACHE AND OP UI
-            await addChatMessage({
-                variables: {
-                    message: {
-                        channelId: id,
-                        text: evt.target.value
-                    }
-                },
-               
-            })
-            evt.target.value = "";
+            await sendMessage();
         }
-    }
+    };
+    const sendMessage = async () => {
+        await addChatMessage({
+            variables: {
+                message: {
+                    channelId: id,
+                    username: name,
+                    text: messageText,
+                },
+            },
+           
+        });
+        setMessageText("");
+    };
+            
     return (
         <>
             {data && Auth.loggedIn() ?(
@@ -57,14 +66,25 @@ export default function Channel() {
                         <p key={msg.id}>{msg.username}: {msg.text}</p>)) : "No messages"}
                     <br />
                     <br />
-           
+
+                    <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+
                 <input
                 type="text"
                 placeholder="+ New message"
+                value={messageText}
+                onChange={(e) => setMessageText(e.target.value)}
                 onKeyUp={handleKeyUp}
             />
+            <button onClick={sendMessage}>Send</button>
                </>
-            ) :(<div>You must be logged in to chat. </div>)}
+            ) :(<div>You must be logged in to chat. </div>
+            )}
             
             {error && <div>ERROR: {error.message}</div>}
         </>
